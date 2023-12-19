5f2e77d0d5cf4be973f1fc01a465545c10b098f5
Matthew Grainger <mattglg@outlook.com>
2023-12-19 13:05:10 +0000

    Updated network.tf

--- vm2022/network.tf
+++ vm2022/network.tf
@@ -1,6 +1,6 @@
 # Create public IPs
 resource "azurerm_public_ip" "pip" {
-  name                = "${var.prefix}-pip"
+  name                = "${var.vmname}-pip"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Dynamic"
@@ -8,7 +8,7 @@ resource "azurerm_public_ip" "pip" {
 
 # Create Network Security Group and rules
 resource "azurerm_network_security_group" "nsg" {
-  name                = "${var.prefix}-nsg"
+  name                = "${var.vmname}-nsg"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
@@ -38,12 +38,12 @@ resource "azurerm_network_security_group" "nsg" {
 
 # Create network interface
 resource "azurerm_network_interface" "nic" {
-  name                = "${var.prefix}-nic"
+  name                = "${var.vmname}-nic"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
-    name                          = "${var.prefix}_nic_configuration"
+    name                          = "${var.vmname}_nic_configuration"
     subnet_id                     = data.azurerm_subnet.snet.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.pip.id
