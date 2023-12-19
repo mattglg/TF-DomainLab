71d276089c0941753b80153a3d3e0b7433228da1
Matthew Grainger <mattglg@outlook.com>
2023-12-19 13:17:14 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+# Create a resource group fot the VM
+resource "azurerm_resource_group" "rg" {
+  location = var.location
+  name     = "${var.envname}-rg"
+}
+
 module "vm1" {
     source = "./vm2022"
+    azurerm_resource_group = azurerm_resource_group.rg
+    vmname = "vm2"
 }
