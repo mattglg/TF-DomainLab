be0ade9325e8c0de034510401617f77e5678e126
Matthew Grainger <mattglg@outlook.com>
2023-12-19 13:19:07 +0000

    Added variables.tf

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "vmname" {
+  type    = string
+  default = "vm1"
+}
+
+variable "vmpassword" {
+  type    = string
+  default = "LabPass264786"
+}
+
+variable "location" {
+  type    = string
+  default = "UK South"
+}
+
+variable "size" {
+  type    = string
+  default = "Standard_DS1_v2"
+}
