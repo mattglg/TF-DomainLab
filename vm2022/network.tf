0619fb583a397b062859913fb2c786a02f071334
Matthew Grainger <mattglg@outlook.com>
2023-12-19 14:19:33 +0000

    Updated network.tf

--- vm2022/network.tf
+++ vm2022/network.tf
@@ -23,17 +23,6 @@ resource "azurerm_network_security_group" "nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
-  security_rule {
-    name                       = "web"
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "80"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
 }
 
 # Create network interface
