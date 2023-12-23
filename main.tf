3ebff4a8267c38c991d436f6ee6d0265da53d66f
mattglg <34393793+mattglg@users.noreply.github.com>
2023-12-23 15:39:21 +0000

    add nsg

--- main.tf
+++ main.tf
@@ -21,6 +21,31 @@ resource "azurerm_subnet" "snet" {
   address_prefixes     = [var.subnet_address_space]
 }
 
+# Create Network Security Group and rules
+resource "azurerm_network_security_group" "nsg" {
+  name                = "${var.envname}-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "RDP"
+    priority                   = 1000
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# Associate the NSG with the subnet
+resource "azurerm_subnet_network_security_group_association" "nsga" {
+  subnet_id                 = azurerm_subnet.snet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
 #Create an availability group fpr the domain contollers
 resource "azurerm_availability_set" "dc-availability-set" {
   name                         = "${var.envname}-dc-avail"
