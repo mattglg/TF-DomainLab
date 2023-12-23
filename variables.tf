95e378ff22114a15a79a4706dbf6d8d04fa2dd1b
mattglg <34393793+mattglg@users.noreply.github.com>
2023-12-23 15:57:22 +0000

    add tfvars file

--- variables.tf
+++ variables.tf
@@ -2,46 +2,38 @@
 
 variable "envname" {
   type    = string
-  default = "testenv1"
 }
 
 variable "member_server_count" {
   type    = number
-  default = "2"
 }
 
 variable "location" {
   type    = string
-  default = "UK South"
 }
 
 variable "size" {
   type    = string
-  default = "Standard_DS1_v2"
 }
 
 variable "vnet_address_space" {
   type        = string
   description = "This variable defines the address space of the vnet via CIDR"
-  default = "10.0.0.0/16"
 }
 
 variable "subnet_address_space" {
   type        = string
   description = "This variable defines the address space of the subnet via CIDR"
-  default = "10.0.1.0/24"
 }
 
 variable "keyvault_name" {
   type        = string
   description = "This variable defines the KeyVault holding the domain and server passwords"
-  default = "kv-terraform-mattglg"
 }
 
 variable "domain_password_secret_name" {
   type        = string
   description = "This variable defines the name of the secret holding the domain password"
-  default = "domainadmin"
 }
 
 # DOMAIN CONTROLLER 1 VARIABLES #
@@ -50,14 +42,12 @@ variable "domain_password_secret_name" {
 variable "ad_dc1_name" {
   type        = string
   description = "This variable defines the name of AD Domain Controller 1"
-  default = "dc1"
 }
 
 # domain controller 1 private ip address
 variable "ad_dc1_ip_address" {
   type        = string
   description = "This variable defines the private ip address of AD Domain Controller 1"
-  default = "10.0.1.10"
 }
 
 # DOMAIN CONTROLLER 2 VARIABLES #
@@ -66,14 +56,12 @@ variable "ad_dc1_ip_address" {
 variable "ad_dc2_name" {
   type        = string
   description = "This variable defines the name of AD Domain Controller 2"
-  default = "dc2"
 }
 
 # domain controller 2private ip address
 variable "ad_dc2_ip_address" {
   type        = string
   description = "This variable defines the private IP address of AD Domain Controller 2"
-  default = "10.0.1.11"
 }
 
 # ACTIVE DIRECTORY CONFIGURATION #
@@ -87,61 +75,40 @@ locals {
 variable "ad_domain_name" {
   type        = string
   description = "This variable defines the name of Active Directory domain, for example kopicloud.local"
-  default = "badmin.co.uk"
 }
 
 # active directory domain NetBIOS name
 variable "ad_domain_netbios_name" {
   type        = string
   description = "This variable defines the NETBIOS name of Active Directory domain, for example kopicloud"
-  default = "BADMIN"
 }
 
 # active directory domain mode
 variable "ad_domain_mode" {
   type        = string
   description = "This variable defines the mode of Active Directory domain and forest functional level"
-  default     = "WinThreshold" # Windows Server 2016
 }
 
 # local administrator username
 variable "ad_admin_username" {
   type        = string
   description = "The username associated with the local administrator account on the virtual machine"
-  default = "mattglg"
-}
-
-# local administrator password
-variable "ad_admin_password" {
-  type        = string
-  description = "The password associated with the local administrator account on the virtual machine"
-  default = "LabPass264786"
 }
 
 # active directory database path
 variable "ad_database_path" {
   type        = string
   description = "The active directory database path"
-  default     = "C:/Windows/NTDS"
 }
 
 # active directory sysvol path
 variable "ad_sysvol_path" {
   type        = string
   description = "The active directory SYSVOL path"
-  default     = "C:/Windows/SYSVOL"
 }
 
 # active directory log path
 variable "ad_log_path" {
   type        = string
   description = "The active directory log path"
-  default     = "C:/Windows/NTDS"
-}
-
-# active directory safe mode administrator password
-variable "ad_safe_mode_administrator_password" {
-  type        = string
-  description = "The active directory safe mode administrator password"
-  default = "LabPass264786"
 }
